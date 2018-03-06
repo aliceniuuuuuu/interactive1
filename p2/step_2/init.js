26ebafb673279ae696e7ff18baa53b310c17fbcc
Alice <yufanniu@cca.edu>
2018-03-05 23:45:07 -0800

    p2 step2

---
+++ p2/step_2/init.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+$('#button-1').click(function(){
+   $('.red').addClass('show');});
+
+$('#button-2').click(function(){
+   $('.orange').addClass('show');
+});
+
+$('#button-3').click(function(){
+   $('.yellow').addClass('show');
+});
+
+$('#button-4').click(function(){
+   $('.green').addClass('show');
+});
+
+$('#button-5').click(function(){
+   alert("There is only one buy button in cyan.");
+});
+
+$('#button-6').click(function(){
+   $('.blue').addClass('show');
+});
+
+$('#button-7').click(function(){
+   $('.grey').addClass('show');
+});
+
+$('#button-8').click(function(){
+   $('.black').addClass('show');
+});
+
+});
