e8875a70247107ff81f5a45f03e7c1264d5e4e2f
Alice <yufanniu@cca.edu>
2018-03-15 13:07:05 -0700

    step 3

--- p2/step_3/init.js
+++ p2/step_3/init.js
@@ -5,26 +5,42 @@ $(document).ready(function(){
      
 //     });  
 
-$("img").hover(function() {
-    $(this).css('cursor','pointer');
-}, function() {
-    $(this).css('cursor','auto');
+// $("img").hover(function() {
+//     $(this).css('cursor','pointer');
+// }, function() {
+//     $(this).css('cursor','auto');
+// });
+
+var n = 1;
+
+    $('#button').click(function() {
+        $('img:eq('+n+')').addClass('show');
+        n++;
+    });
+
+	// $('#button').click(function() {
+ //        $('img').addClass('show');
+ //    });  
+
+
+	// store the height and width of the window as variables
+	var winHeight = $(window).height();
+	var winWidth = $(window).width();
+
+	// loop over each image on the page
+	$('img').each(function() {
+
+		// get a random x and y position
+		var randPosX = Math.floor((Math.random()*winWidth));
+		var randPosY = Math.floor((Math.random()*winHeight));
+
+		// apply the random position
+		$(this).css('left', (randPosX)+'px');
+		$(this).css('top', (randPosY)+'px');
+	});
+
 });
 
-// var n = 1;
-
-//     $('img').click(function() {
-//         $('img:eq('+n+')').show();
-//         n++;
-//     });
-
-$('#button').click(function() {
-        $('img').addClass('show');
-     
-    });  
-
-
-});
 
 
 
